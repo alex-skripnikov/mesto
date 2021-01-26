31d2b52845d8c065bab49acb902c1d75ff6037b0
Alex Skripnikov <a.p.skripnikov@gmail.com>
2021-01-26 17:27:04 +0300

    version 2 fix review bag

--- script/script.js
+++ script/script.js
@@ -1,6 +1,42 @@
+//Переменные
 let overlay = document.querySelector('.overlay');
 let openEditButton = document.querySelector('.profile__editButton');
 let closeEditButton = overlay.querySelector('.popup__closeButton');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = overlay.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_value_name');
+let jobInput = formElement.querySelector('.popup__input_value_job');
+
+//Функция открытия popup с внесение данных в форму
+function openpopup() {
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+    overlay.classList.add('overlay_active')
+}
+
+//Функция закрытия popup без сохранения данных
+function closepopup() {
+    overlay.classList.remove('overlay_active')
+}
+
+//Функция закрытия popup с сохранением данных
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+
+    closepopup();
+}
+
+
+//слушатели
+openEditButton.addEventListener('click', openpopup);
+closeEditButton.addEventListener('click', closepopup);
+formElement.addEventListener('submit', formSubmitHandler);
+/*
+
 
 function togglepopup() {
     overlay.classList.toggle('overlay_active')
@@ -13,16 +49,7 @@ let formElement = overlay.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__nameInput');
 let jobInput = formElement.querySelector('.popup__jobInput');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    
-    let profileTitle = document.querySelector('.profile__title');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
 
-    profileTitle.textContent = nameInput.value;
-    profileSubtitle.textContent = jobInput.value;
 
-    togglepopup();
-}
 
-formElement.addEventListener('submit', formSubmitHandler);
+*/
