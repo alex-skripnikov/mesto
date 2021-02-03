de4846eed8248bf4dc45631b473462d1c58433b5
Alex Skripnikov <a.p.skripnikov@gmail.com>
2021-02-03 12:41:40 +0300

    version 3-4 fix review bags

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 //Переменные
 const overlayEdit = document.querySelector('.overlay_type_edit'); // секция попапа редактирования
 const openEditButton = document.querySelector('.profile__editButton'); // кнопка открытия попапа редактирования
-const closeEditButton = overlayEdit.querySelector('.popup__closeButton'); // кнопка закрытия попапа редактирования
 const profileTitle = document.querySelector('.profile__title'); // текущее имя для попапа редактирования
 const profileSubtitle = document.querySelector('.profile__subtitle'); // текущая работа для попапа редактирования
 const formEditPopup = overlayEdit.querySelector('.popup__form'); // форма попапа редактирования
@@ -10,7 +9,6 @@ const jobInput = formEditPopup.querySelector('.popup__input_value_job'); // по
 
 const overlayAddPlace = document.querySelector('.overlay_type_addPlace'); // секция попапа добавления места
 const openAddButton = document.querySelector('.profile__addButton'); // кнопка открытия попапа добавления места
-const closeAddBoxButton = overlayAddPlace.querySelector('.popup__closeButton'); // кнопка закрытия попапа добавления места
 const formAddPopup = overlayAddPlace.querySelector('.popup__form'); // форма попапа добавления места
 const namePlace = formAddPopup.querySelector('.popup__input_value_namePlace'); // поле имя в форме добавления места
 const imgPlace = formAddPopup.querySelector('.popup__input_value_imgPlace'); // поле картинка в форме добавления места
@@ -18,10 +16,23 @@ const imgPlace = formAddPopup.querySelector('.popup__input_value_imgPlace'); // 
 const overlayPictureBox = document.querySelector('.overlay_type_image'); // секция попапа большой картинки
 const overlayPicture = overlayPictureBox.querySelector('.popup__piture'); // картинка попапа большой картинки
 const overlayPictureName = overlayPictureBox.querySelector('.popup__pitureName'); // подпись попапа большой картинки
-const closePictureButton = overlayPictureBox.querySelector('.popup__closeButton'); // кнопка закрытия попапа редактирования
 
-const elementsBox = document.querySelector('.elements'); //общий контейнер для мест, сюда добавляем места функцией addplace
-const placeTemplate = elementsBox.querySelector('.elements__articleTemplate').content; //шаблон для мест используем в функции addplace
+const elementsBox = document.querySelector('.elements'); //общий контейнер для мест, сюда добавляем места функцией addPlace
+const placeTemplate = elementsBox.querySelector('.elements__articleTemplate').content; //шаблон для мест используем в функции addPlace
+
+const popups = document.querySelectorAll('.overlay')//массив всех popup с overlay на странице
+
+//добавляем обработчик клика всем popup с overlay на странице и закрываем если кликают по крестику или overlay_active
+popups.forEach((popup) => {
+    popup.addEventListener('click', (evt) => {
+        if (evt.target.classList.contains('overlay_active')) {
+            closePopup(popup)
+        }
+        if (evt.target.classList.contains('popup__closeButton')) {
+          closePopup(popup)
+        }
+    })
+})
 
 //функция создания элемента нового места
 function getCardElement(name, link) {
@@ -34,13 +45,13 @@ function getCardElement(name, link) {
 }
 
 //функция добавления места
-function addplace(name, link) {
+function addPlace(name, link) {
     elementsBox.prepend(getCardElement(name, link));
 };
 
 //выводим массив с базовыми местами
 initialCards.forEach(function (item) {
-    addplace(item.name, item.link);
+    addPlace(item.name, item.link);
 });
 
 //Функция внесения данных в форму перед открытием попапа редактирования
@@ -53,11 +64,13 @@ function addValuePopup() {
 //Функция открытия popup
 function openPopup(type) {
     type.classList.add('overlay_active');
+    document.addEventListener('keydown', closePopupByEsc);
 }
 
 //Функция закрытия popup без сохранения данных
 function closePopup(type) {
-    type.classList.remove('overlay_active')
+    type.classList.remove('overlay_active');
+    document.removeEventListener('keydown', closePopupByEsc);
 }
 
 //Функция закрытия popup редактирования с сохранением данных
@@ -77,30 +90,22 @@ function createPlace (evt) {
     evt.preventDefault(); //оменяем стандартную обработку submit
     const lokalFormButton = evt.target.querySelector('.popup__saveButton');
     if (!lokalFormButton.classList.contains('popup__saveButton_disabled')) {
-        addplace(namePlace.value, imgPlace.value);
+        addPlace(namePlace.value, imgPlace.value);
         formAddPopup.reset();
         lokalFormButton.classList.add('popup__saveButton_disabled');
         closePopup(overlayAddPlace);
     };
 }
 
-//функция закрытия popup по клику на overlay
-function closePopupByOverlay(evt, type) {
-    if (evt.target === type) {
-        closePopup(type);
-    }
-}
-
 //функция закрытия popup по кнопке Esc
 function closePopupByEsc(evt) {
     if (evt.key === 'Escape') {
-        closePopup(overlayEdit);
-        closePopup(overlayAddPlace);
-        closePopup(overlayPictureBox);
+        const openedPopup = document.querySelector('.overlay_active')
+        closePopup(openedPopup);
     }
 }
 
-//функция удаления 
+//функция удаления места
 function deletePlace(evt) {
     evt.target.closest('.element').remove();
 }
@@ -133,14 +138,7 @@ function processingClickElementBox(evt) {
 
 //слушатели
 openAddButton.addEventListener("click", () => openPopup(overlayAddPlace));
-closeAddBoxButton.addEventListener("click", () => closePopup(overlayAddPlace));
 openEditButton.addEventListener("click", () => addValuePopup());
-closeEditButton.addEventListener("click", () => closePopup(overlayEdit));
 formEditPopup.addEventListener('submit', changeFormEdit);
 formAddPopup.addEventListener('submit', createPlace);
-closePictureButton.addEventListener("click", () => closePopup(overlayPictureBox));
-overlayEdit.addEventListener("click", (evt) => closePopupByOverlay(evt, overlayEdit));
-overlayAddPlace.addEventListener("click", (evt) => closePopupByOverlay(evt, overlayAddPlace));
-overlayPictureBox.addEventListener("click", (evt) => closePopupByOverlay(evt, overlayPictureBox));
-document.body.addEventListener("keydown", (evt) => closePopupByEsc(evt));
 elementsBox.addEventListener("click", (evt) => processingClickElementBox(evt));
