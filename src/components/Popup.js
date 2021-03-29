9dcc5b3c04e2cea06e2bad70c5e76c854ca2964b
Alex Skripnikov <a.p.skripnikov@gmail.com>
2021-03-29 12:10:21 +0300

    sprint 8 version 3.0 fix review bag - Listener

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,22 +2,23 @@ export class Popup {
 	constructor(popupSelector) {
     this._popupSelector = popupSelector;
     this._popupElement = document.querySelector(this._popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this)
 	}
 
 	//публичный метод отвечающий за открытие Popup
     open() {
         this._popupElement.classList.add('overlay_active');
-        document.addEventListener('keydown', this._handleEscClose.bind(this));
+        document.addEventListener('keydown', this._handleEscClose);
     }
 
 	//публичный метод отвечающий за закрытие Popup
     close() {
         this._popupElement.classList.remove('overlay_active');
-        document.removeEventListener('keydown', this._handleEscClose.bind(this));
+        document.removeEventListener('keydown', this._handleEscClose);
     }
 
     //приватный метод отвечающий за закрытие Popup по кнопке Esc
-    _handleEscClose() {
+    _handleEscClose(event) {
         if (event.code === 'Escape') { this.close(); }
     } 
 
